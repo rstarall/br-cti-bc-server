bd6aab2adbbba30651124a805b9a759f5339966f
right star <1145133484@qq.com>
2024-12-13 11:23:33 +0800

    update

---
+++ service/ipfs_service.go
@@ -0,0 +1,35 @@
+
+
+type IPFSService struct {
+}
+
+func (s *IPFSService) DownloadFile(hash string) (string, error) {
+
+}
+
+func (s *IPFSService) GetIOCFileFromIPFS(IPFSHash string) (string, error) {
+
+}
+//----------------------------------IOC地理位置信息------------------------------------
+func (s *IPFSService) ProcessIOCWorldMapStatistics(iocData string) (string, error) {
+	// 处理IOC世界地图统计数据
+}
+func (s *IPFSService) SaveIOCWorldMapStatistics(iocData string) (string, error) {
+	// 保存IOC世界地图统计数据(保存在数据库或文件中)
+}
+func (s *IPFSService) GetIOCWorldMapStatistics() (string, error) {
+	// 获取IOC世界地图统计数据
+}
+
+
+
+//----------------------------------IOC统计信息------------------------------------
+func (s *IPFSService) ProcessIOCStatistics(iocData string) (string, error) {
+	// 处理IOC统计信息
+}
+func (s *IPFSService) SaveIOCStatistics(iocData string) (string, error) {
+	// 保存IOC统计信息(保存在数据库或文件中)
+}
+func (s *IPFSService) GetIOCStatistics() (string, error) {
+	// 获取IOC统计信息
+}
